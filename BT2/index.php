87523b1a2cfafc2f20c6f71ef508d0991a6749e2
khoantd <khoantd@smartosc.com>
2020-09-09 18:32:06 +0700

    PHP Exam

--- BT2/index.php
+++ BT2/index.php
@@ -2,7 +2,7 @@
 // In ra tất cả các số lặp lại trên 3 lần từ file input.txt
 $myfile = fopen("input.txt", "r") or die("Unable to open file!");//Open file
 $text_array = preg_split("/[\s,]+/",fread($myfile,filesize("input.txt")));// Delete downline and read file input.txt and add to the array
-$count = array_count_values($text);// count the number of appearance's number
+$count = array_count_values($text_array);// count the number of appearance's number
 $number_count = array();// add result of count number
 foreach ($count as $key=>$value){
     if($value >= 3){
@@ -10,4 +10,4 @@ foreach ($count as $key=>$value){
     }
 }
 $output = implode(',',$number_count);// implode an array to string
-echo 'Number appearance more than 3 times is:'.$output.' ';
+echo "the number apearance 3 times is:". $output;
